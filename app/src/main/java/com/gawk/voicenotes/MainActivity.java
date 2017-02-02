c4f77c3da29e6f802e3dbcd83d47fa7125b6e6a2
dartl <twisan@mail.ru>
2017-02-02 16:30:10 +0300

    Добавлены переменные

--- app/src/main/java/com/gawk/voicenotes/MainActivity.java
+++ app/src/main/java/com/gawk/voicenotes/MainActivity.java
@@ -17,4 +17,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    private void setupViewPager(ViewPager viewPager) {
+    }
 }
