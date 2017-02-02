ec0f22afc6df3e45d30e5af439e08290e9ac7f67
dartl <twisan@mail.ru>
2017-02-02 16:28:43 +0300

    Добавлены переменные

--- app/src/main/java/com/gawk/voicenotes/MainActivity.java
+++ app/src/main/java/com/gawk/voicenotes/MainActivity.java
@@ -1,9 +1,16 @@
 package com.gawk.voicenotes;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.TableLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private TableLayout tabLayout;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
